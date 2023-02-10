3929c128c5560e420a86961586792fa037434f25
Anton Kargapolov <igloooooook@gmail.com>
2023-02-10 19:19:03 +0500

    task dz 019

---
+++ dz_task_019/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 19: Напишите программу, которая принимает 
+// на вход пятизначное число и проверяет, является ли 
+// оно палиндромом.
+
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
+
+
+int InputNumber(string str)
+{
+    System.Console.WriteLine(str);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int x = InputNumber("Введите пятизначное число ");
+
+int[] arr = new int[5];
+if (x>9999 && x<100000)
+{
+    for (int i = 4; i >= 0; i--)
+    {
+        arr[i] = x % 10;
+        x = x / 10;
+    }
+
+
+    if (arr[0] == arr[4] && arr[1] == arr[3])
+    {
+        System.Console.WriteLine("Да");
+    }
+    else
+    {
+        System.Console.WriteLine("Нет");
+    }
+}
+else
+{
+    System.Console.WriteLine("Число не является пятизначным");
+}    
